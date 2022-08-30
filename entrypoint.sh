139665037d83b6ee1da04dd30c453a1105a83575
Matt Withoos <mattwithoos@gmail.com>
2022-08-30 12:25:34 +1000

    snatch the error code from the trap frame

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,15 @@ echo "Target directory: $TARGET_DIR"
 wget -qO- https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2Fv${KUSTOMIZE_VER}/kustomize_v${KUSTOMIZE_VER}_linux_amd64.tar.gz | tar xvz -C /usr/bin
 chmod u+x /usr/bin/kustomize
 
-build_output=`/usr/bin/kustomize build ${TARGET_DIR} | kubeval`
-echo $build_output
+echo "::set-output name=build_output::"
+touch kusteval.yaml
+/usr/bin/kustomize build ${TARGET_DIR} > kusteval.yaml
+cat kusteval.yaml
+echo
 
-echo "::set-output name=build_output::$build_output"
+echo "::set-output name=eval_output::"
+kubeval --strict kusteval.yaml
+
+
+
+
